c78559f18a9fb8e2061cd1356f21da0496573ed0
Kyrie Williams <wkyrie98@gmail.com>
2024-09-09 16:12:54 -0500

    Assessment code complete

--- main.js
+++ main.js
@@ -33,6 +33,6 @@ for (const drover of drovers) {
 }
 
 console.log("\n\nYour journey will take you through the wildness of the American Midwest and across the following terrain")
-for (const area of journey) {
-    console.log(`\t* ${area}`)
+for (const areas of journey) {
+    console.log(`\t* ${areas}`)
 }
